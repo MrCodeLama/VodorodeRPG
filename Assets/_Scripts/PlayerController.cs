835d6581f6753792b583bde0ceaef5a50d94f6d5
Volodymyr Demianchuk <dem.volodymyrrr@gmail.com>
2024-03-29 09:32:19 +0200

    playerControll+camera

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 4f;
     public Transform movePoint;
-    public Animator anim;
+    public Animator animator;
     public LayerMask whatStopsMovement;
     // Start is called before the first frame update
     void Start()
@@ -21,36 +21,34 @@ public class PlayerController : MonoBehaviour
 
         if (Vector3.Distance(transform.position, movePoint.position) <= 0.01f)
         {
+            
             if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
             {
-                if (Input.GetAxisRaw("Horizontal") < 0)
-                {
-                    transform.localScale = new Vector3(-1, 1, 1);
-                }
-                if (Input.GetAxisRaw("Horizontal") > 0)
-                {
-                    transform.localScale = new Vector3(1, 1, 1);
-                }
+                //animator.SetFloat("Horizontal", Input.GetAxisRaw("Horizontal"));
+                //animator.SetFloat("Vertical", 0);
+
                 if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f),
                         0.2f, whatStopsMovement))
                 {
                     movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
                 }
-                
+
             }
             else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
             {
+                //animator.SetFloat("Vertical", Input.GetAxisRaw("Vertical"));
+                //animator.SetFloat("Horizontal", 0);
                 if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f),
                         0.2f, whatStopsMovement))
                 {
                     movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
                 }
             }
-            anim.SetBool("moving", false);
-        }
-        else
-        {
-            anim.SetBool("moving", true);
+            else
+            {
+                //animator.SetFloat("Vertical", 0);
+                //animator.SetFloat("Horizontal", 0);
+            }
         }
     }
 }
