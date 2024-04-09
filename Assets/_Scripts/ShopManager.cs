2ab5f58bd7b675cb9887e919c3ca4cc5e31d1c5b
jabka0 <136057427+Jabka00@users.noreply.github.com>
2024-04-09 22:21:45 +0300

    add shop tab

--- Assets/_Scripts/ShopManager.cs
+++ Assets/_Scripts/ShopManager.cs
@@ -8,13 +8,16 @@ public class ShopManager : MonoBehaviour
 {
    public static ShopManager instance;
    public int coins = 300;
-   //public Upgrade[] upgrades;
+   public Upgrade[] upgrades;
+   
    public Text coinText;
    public GameObject shopUI;
-
+   public Transform shopContent;
+   public GameObject itemPrefab;
+   //public PlayerMovement player;
    private void Awake()
    {
-      if (instance==null)
+      if (instance == null)
       {
          instance = this;
       }
@@ -25,8 +28,53 @@ public class ShopManager : MonoBehaviour
       DontDestroyOnLoad(gameObject);
    }
 
+   public void Start()
+   {
+      foreach (Upgrade upgrade in upgrades)
+      {
+         GameObject item = Instantiate(itemPrefab, shopContent);
+
+         upgrade.itemRef = item;
+
+         foreach (Transform child in item.transform)
+         {
+            if (child.gameObject.name == "Quantity")
+            {
+               child.gameObject.GetComponent<Text>().text = upgrade.quantity.ToString();
+            }
+            else if (child.gameObject.name == "Cost")
+            {
+               child.gameObject.GetComponent<Text>().text = "Price: " + upgrade.cost.ToString();
+            }
+            else if (child.gameObject.name == "Title")
+            {
+               child.gameObject.GetComponent<Text>().text = upgrade.name;
+            }
+            else if (child.gameObject.name == "Image")
+            {
+               child.gameObject.GetComponent<Image>().sprite = upgrade.image;
+            }
+         }
+      }
+   }
+
    public void ToggleShop()
    {
       shopUI.SetActive(!shopUI.activeSelf);
    }
+
+   public void Update()
+   {
+      coinText.text = "Coins: " + coins.ToString();
+   }
+}
+
+[System.Serializable]
+public class Upgrade
+{
+   public string name;
+   public int cost;
+   public Sprite image;
+   [HideInInspector] public int quantity;
+   [HideInInspector] public GameObject itemRef;
 }
