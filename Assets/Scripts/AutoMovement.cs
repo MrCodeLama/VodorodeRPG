ae5963be27ecd953ea1e9a249b334f226f03dbf4
jabka0 <136057427+Jabka00@users.noreply.github.com>
2024-04-25 19:44:42 +0300

    minor changes

--- Assets/Scripts/AutoMovement.cs
+++ Assets/Scripts/AutoMovement.cs
@@ -1,12 +1,19 @@
 using UnityEngine;
+using System.Collections;
 
 public class AutoMovement : MonoBehaviour
 {
     public float speed = 1f; 
     public float distance = 2.7f; 
+    public float highSpeed = 15f; 
+    public float delayBeforeHighSpeed = 5f; 
+    public float fallDistance = 100f; 
 
     private Vector3 initialPosition;
     private bool movingRight = true;
+    private bool moveDown = false;
+    private bool falling = false;
+    private float fallStartPosition;
 
     private SpriteRenderer spriteRenderer;
 
@@ -14,35 +21,59 @@ public class AutoMovement : MonoBehaviour
     {
         initialPosition = transform.position;
         spriteRenderer = GetComponent<SpriteRenderer>();
+        StartCoroutine(StartHighSpeedAfterDelay());
     }
 
     void Update()
     {
-        if (movingRight && transform.position.x >= initialPosition.x + distance)
+        if (falling)
         {
-            movingRight = false;
-            FlipSprite();
-        }
-        else if (!movingRight && transform.position.x <= initialPosition.x)
-        {
-            movingRight = true;
-            FlipSprite();
-        }
+            transform.Translate(Vector2.down * highSpeed * Time.deltaTime);
 
-        // Движение вправо
-        if (movingRight)
-        {
-            transform.Translate(Vector2.right * speed * Time.deltaTime);
+            if (transform.position.y <= fallStartPosition - fallDistance)
+            {
+                falling = false;
+                moveDown = false;
+            }
         }
-        // Движение влево
+        else if (moveDown)
+        {
+            transform.Translate(Vector2.down * highSpeed * Time.deltaTime);
+        }
         else
         {
-            transform.Translate(Vector2.left * speed * Time.deltaTime);
+            if (movingRight && transform.position.x >= initialPosition.x + distance)
+            {
+                movingRight = false;
+                FlipSprite();
+            }
+            else if (!movingRight && transform.position.x <= initialPosition.x)
+            {
+                movingRight = true;
+                FlipSprite();
+            }
+
+            if (movingRight)
+            {
+                transform.Translate(Vector2.right * speed * Time.deltaTime);
+            }
+            else
+            {
+                transform.Translate(Vector2.left * speed * Time.deltaTime);
+            }
         }
     }
 
     void FlipSprite()
     {
         spriteRenderer.flipX = !spriteRenderer.flipX;
+    }
+
+    IEnumerator StartHighSpeedAfterDelay()
+    {
+        yield return new WaitForSeconds(delayBeforeHighSpeed);
+        moveDown = true;
+        fallStartPosition = transform.position.y;
+        falling = true;
     }
 }
